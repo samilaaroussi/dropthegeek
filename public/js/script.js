9c001368703cb094cce929a04e935b6357bfa6d4
Sami <sami@slt.fr>
2016-05-09 20:15:46 +0200

    Changement du jeu principal: objets geek dans un salon

--- public/js/script.js
+++ public/js/script.js
@@ -4,11 +4,11 @@
 var Dragging = new Kiwi.State('Play');
 
 Dragging.preload = function () {
-    this.game.stage.height = 250;
-    this.addImage('kitchen', 'assets/static/kitchen2.jpg');
-    this.addImage('wok', 'assets/static/wok.png');
-    this.addImage('ramen', 'assets/static/ramen.png');
-    this.addImage('soja', 'assets/static/Soy_sauce.png');
+    this.game.stage.height = 480;
+    this.addImage('room', 'assets/static/room.png');
+    this.addImage('pikachu', 'assets/static/pikachu.png');
+    this.addImage('superman', 'assets/static/superman.png');
+    this.addImage('bloc', 'assets/static/bloc.png');
 }
 
 Dragging.create = function () {
@@ -17,39 +17,44 @@ Dragging.create = function () {
     text.textAlign = 'center';
     this.addChild(text);
 
-    //wallpaper
-    this.kitchen = new Kiwi.GameObjects.Sprite(this, this.textures.kitchen, 0, 0);
-    this.addChild(this.kitchen);
+    //Background
+    this.room = new Kiwi.GameObjects.Sprite(this, this.textures.room, 0, 0);
+    this.addChild(this.room);
 
-    //Create the wok. Enable the input component by passing true.
-    this.wok = new Kiwi.GameObjects.Sprite(this, this.textures.wok, 400, 300, true);
-    this.addChild(this.wok);
+    //Create the pikachu. Enable the input component by passing true.
+    this.pikachu = new Kiwi.GameObjects.Sprite(this, this.textures.pikachu, 660, 162, true);
+    this.addChild(this.pikachu);
 
-    //Create the ramen. Enable the input component by passing true.
-    this.ramen = new Kiwi.GameObjects.Sprite(this, this.textures.ramen, 10, 10, true);
-    this.addChild(this.ramen);
+    //Create the superman. Enable the input component by passing true.
+    this.superman = new Kiwi.GameObjects.Sprite(this, this.textures.superman, 10, 10, true);
+    this.addChild(this.superman);
 
-    //Create the soy-sauce.
-    this.soja = new Kiwi.GameObjects.Sprite(this, this.textures.soja, 800, 300, true);
-    this.addChild(this.soja);
+    //Create a Mario Bros block
+    this.bloc = new Kiwi.GameObjects.Sprite(this, this.textures.bloc, 400, 300, true);
+    this.addChild(this.bloc);
 
     /**
      * When you want a sprite to be draggable you have to enable the drag on that element.
      **/
-    this.ramen.input.enableDrag();
-    /* this.ramen.input.onDragStarted.add( this.startedDrag, this );
-     this.ramen.input.onDragStopped.add( this.stoppedDrag, this );*/
 
-    this.soja.input.enableDrag()
-    /* this.ramen.input.onDragStarted.add( this.startedDrag, this );
-     this.ramen.input.onDragStopped.add( this.stoppedDrag, this );*/
+    this.pikachu.input.enableDrag();
+    /* this.superman.input.onDragStarted.add( this.startedDrag, this );
+     this.superman.input.onDragStopped.add( this.stoppedDrag, this );*/
+
+    this.superman.input.enableDrag();
+    /* this.superman.input.onDragStarted.add( this.startedDrag, this );
+     this.superman.input.onDragStopped.add( this.stoppedDrag, this );*/
+
+    this.bloc.input.enableDrag()
+    /* this.superman.input.onDragStarted.add( this.startedDrag, this );
+     this.superman.input.onDragStopped.add( this.stoppedDrag, this );*/
 
 
     /**
      * Parameter One - OPTIONAL - Snap the sprite to the center. - Default to false
      * Parameter Two - OPTIONAL - Distance between gridpoints in which the sprite should snap to.
      **/
-    //this.soja.input.enableDrag(true, 25);
+    //this.bloc.input.enableDrag(true, 25);
 }
 
 Dragging.startedDrag = function () {
