e9745a913ad6b56a73f859d25bd62dd9b4c7aec0
dimical <dimfox@live.fr>
2016-05-30 18:15:11 +0200

    ok check la catégorie sur le nom

--- public/js/script.js
+++ public/js/script.js
@@ -39,12 +39,13 @@ function create() {
     var biblio = new Phaser.Physics.Box2D.Body(this.game, null, 670, 300, 100);
     biblio.setPolygon([-40, -100, -40, 150, 40, 150, 40, -100 ]);
 
-    var carton = game.add.sprite(350, 420, 'carton');
-    game.physics.box2d.enable(carton);
-    carton.body.fixedRotation = true;
-    //carton.setCircle(50);
-    carton.body.static = true;
+    var boxPokemon = game.add.sprite(350, 420, 'carton');
+    game.physics.box2d.enable(boxPokemon);
+    boxPokemon.body.fixedRotation = true;
+    //boxPokemon.setCircle(50);
+    boxPokemon.body.static = true;
 
+    /*
     var pokemon = game.add.group();
     pokemon.enableBody = true;
     pokemon.physicsBodyType = Phaser.Physics.BOX2D;
@@ -56,26 +57,35 @@ function create() {
     var notGeek = game.add.group();
     notGeek.enableBody = true;
     notGeek.physicsBodyType = Phaser.Physics.BOX2D;
+    */
+
+    var obj = game.add.group();
+    obj.enableBody = true;
+    obj.physicsBodyType = Phaser.Physics.BOX2D;
 
     // on cree chaque object  !
     $.each(nameObjects, function( index, value ) {
 
+        /*
         if(value.includes("pokemon")){
             var object = pokemon.create(Math.floor((Math.random() * game.width) ), Math.floor((Math.random() * game.height) ), value);
-            object.body.setCollisionCategory(1);
         }
         else if(value.includes("nick")){
             var object = nick.create(Math.floor((Math.random() * game.width) ), Math.floor((Math.random() * game.height) ), value);
-            object.body.setCollisionCategory(2);
         }
-        else if(value.includes("notGeek")){
+        else {
             var object = notGeek.create(Math.floor((Math.random() * game.width) ), Math.floor((Math.random() * game.height) ), value);
-            object.body.setCollisionCategory(3);
         }
+        object.body.setCollisionCategory(2);
+        */
+
+
+        var object = obj.create(Math.floor((Math.random() * game.width) ), Math.floor((Math.random() * game.height) ), value);
+        object.body.setCollisionCategory(2);
 
     });
 
-    carton.body.setCategoryContactCallback(2,boxCallback,this);
+    boxPokemon.body.setCategoryContactCallback(2,boxCallback,this);
 
     game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
 
@@ -114,21 +124,34 @@ function boxCallback(body1, body2, fixture1, fixture2, begin) {
         return;
     }
 
+    if(body1.sprite.key=="carton"){
+        if(body2.sprite.key.includes("pokemon")){
+            if (mouseDown){
+                body2.sprite.destroy();
+                console.log("coo");
+            }
+        }
+    }
 
-    console.log(body1,body2);
+
+
+
+
+
+
+
+
+
+
     // body1 is the ship because it's the body that owns the callback
     // body2 is the body it impacted with, in this case the health
     // fixture1 is the fixture of body1 that was touched
     // fixture2 is the fixture of body2 that was touched
 
     // Only pick up health when not at full health
-    if (mouseDown){
-        body2.sprite.destroy();
-    }
 
 
 
-    console.log("on est dans le callback");
 
 }
 
